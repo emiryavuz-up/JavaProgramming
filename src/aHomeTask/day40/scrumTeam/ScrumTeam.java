74ba18fad1443f6cd0251fe4734decc264c10ed9
emiryavuz-up <hemiry96@gmail.com>
2023-08-13 21:56:54 +0200

    Scrum Team Task

--- src/aHomeTask/day40/scrumTeam/ScrumTeam.java
+++ src/aHomeTask/day40/scrumTeam/ScrumTeam.java
@@ -5,12 +5,44 @@ import java.util.Arrays;
 
 public class ScrumTeam {
 
-    public ProductOwner PO;
-    public BusinessAnalyst BA;
-    public ScrumMaster SM;
+    private ProductOwner PO;
+    private BusinessAnalyst BA;
+    private ScrumMaster SM;
 
-    public ArrayList<Tester> testers = new ArrayList<>();
-    public ArrayList<Developer> developers = new ArrayList<>();
+    private ArrayList<Tester> testers = new ArrayList<>();
+    private ArrayList<Developer> developers = new ArrayList<>();
+
+    public ProductOwner getPO() {
+        return PO;
+    }
+
+    public void setPO(ProductOwner PO) {
+        this.PO = PO;
+    }
+
+    public BusinessAnalyst getBA() {
+        return BA;
+    }
+
+    public void setBA(BusinessAnalyst BA) {
+        this.BA = BA;
+    }
+
+    public ScrumMaster getSM() {
+        return SM;
+    }
+
+    public void setSM(ScrumMaster SM) {
+        this.SM = SM;
+    }
+
+    public ArrayList<Tester> getTesters() {
+        return new ArrayList<>(testers); // Return a copy of the testers list
+    }
+
+    public ArrayList<Developer> getDevelopers() {
+        return new ArrayList<>(developers); // Return a copy of the developers list
+    }
 
     public void addTester(Tester tester){
         testers.add(tester);
